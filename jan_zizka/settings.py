cab8f52b8d0ae0b18fab30996076f74f0851b1af
Markus Varckin <varckin@protonmail.com>
2026-04-13 17:26:06 +0300

    add cache redis and update requirements

--- jan_zizka/settings.py
+++ jan_zizka/settings.py
@@ -129,6 +129,16 @@ else:
         },
     }
 
+    CACHES = {
+        "default": {
+            "BACKEND": "django_redis.cache.RedisCache",
+            "LOCATION": "redis://chatredis:6379/1",
+            "OPTIONS": {
+                "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            }
+        }
+    }
+
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
